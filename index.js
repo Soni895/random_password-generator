97c95b7ac3be45c7d6bede2d3385fecccf9ad358
soni895 <darshansoni895@gmail.com>
2023-06-03 15:15:03 +0530

    variable added

--- index.js
+++ index.js
@@ -9,3 +9,9 @@ let Generate_password=document.querySelector("[Generate_password]");
 let Indicator=document.querySelector("[indicator]");
 let Strength=document.querySelector("[Strength]");
 
+let Password="";
+let Plength=10;
+let Check_count=1;
+
+
+
