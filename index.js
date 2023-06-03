5b4a4ba7ccc6e9e02783969c024b95299e4e4bd7
soni895 <darshansoni895@gmail.com>
2023-06-03 22:09:29 +0530

    calculatestrength

--- index.js
+++ index.js
@@ -70,7 +70,43 @@ return Symbole[no];
 }
 function calculatestrength()
 {
+   let upper=0,lower=1,number=1,symbole=1;
+   if(Include_lowercase.Checked)lower=1;
+   if(Include_uppercase.Checked)upper=1;
+   if(Include_sumbols.Checked)symbol=1;
+   if(Include_number.Checked)number=1;
+
+   if(upper&&lower&&symbole&&number){
+    return "strong";
+
+   }
+   else if((upper&&lower&&symbole)||(upper&&lower&&number)||(upper&&symbole&&number)||(symbole&&lower&&number))
+   {
+return "strong";
+   }
+   else
+   if((upper&&lower)||(upper&&number)||(upper&&symbole))
+   {
+    return "medium";
+   }
+   else
+   if((number&&lower)||(symbole&&number))
+   {
+    return "medium";
+   }
+   else
+   if((number&&symbole))
+   {
+    return "medium";
+   }
+   else{
+    return "weak";
+   }
+   
+    
 
 }
-
-
+// let content=generatesymbols();
+// console.log(content);
+calculatestrength("91345");
+console.log(calculatestrength());
