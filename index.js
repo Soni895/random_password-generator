6d8a4169c01b3b83d867df9a0f3a2225a873887a
soni895 <darshansoni895@gmail.com>
2023-06-03 15:05:04 +0530

    variable added

---
+++ index.js
@@ -0,0 +1,11 @@
+let Password_length=document.querySelector("[Password_length]");
+let Slider=document.querySelector('[Slider]');
+let Check_list=document.querySelectorAll("#Checklist")
+let Include_number=document.querySelector("#Numbers");
+let Include_sumbols=document.querySelector("#Symbols");
+let Include_uppercase=document.querySelector("#Uppercase");
+let Include_lowercase=document.querySelector("#Lowercase");
+let Generate_password=document.querySelector("[Generate_password]");
+let Indicator=document.querySelector("[indicator]");
+let Strength=document.querySelector("[Strength]");
+
